4aac4f13eb4290bd221985fb4250d0ea68894df5
afuahdodeshi <almo23mt@student.ju.se>
2025-02-03 21:47:37 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 
 import Rat from "../rat.js";
-import grey_Exterminator from "/grey_exterminator.js";
+import grey_Exterminator from "./exterminators/grey_exterminator.js";
 import red_Exterminator from "./exterminators/red_exterminator.js";
-import purple_Exterminator from "/purple_exterminator.js";
-import pink_Exterminator from "/pink_exterminator.js";
+import purple_Exterminator from "./exterminators/purple_exterminator.js";
+import pink_Exterminator from "./exterminators/pink_exterminator.js";
 let x =300;
 let y = 100;
 let boxSize = 50;
